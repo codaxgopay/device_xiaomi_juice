fbf0ba5c049e644b8212b4c463dda94606b586a2
zacky93 <zackyvirgo9@gmail.com>
2021-11-11 16:20:42 +0700

    Juice PPUI BringUp
    Signed-off-by: zacky93 <zackyvirgo9@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_juice.mk
+    $(LOCAL_DIR)/ppui_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_juice-user \
-    aosp_juice-userdebug \
-    aosp_juice-eng
+    ppui_juice-user \
+    ppui_juice-userdebug \
+    ppui_juice-eng
